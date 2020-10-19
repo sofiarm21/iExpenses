af2bfa8477a510c97b35dbd806ba82597a938f13
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-19 00:33:36 -0400

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Sofia Rodriguez Morales on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
