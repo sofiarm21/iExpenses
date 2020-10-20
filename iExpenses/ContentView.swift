439fb2d500dc0968405c565ecf53983c600696b3
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-19 23:56:21 -0400

    First commit

--- iExpenses/ContentView.swift
+++ iExpenses/ContentView.swift
@@ -7,11 +7,45 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List{
+                ForEach(expenses.items, id:\.name){ item in
+                    Text(item.name)
+                }
+                .onDelete(perform: deleteExpense)
+            }
+            .navigationTitle("iExpenses")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    var expense = ExpenseItem(name:"Item", type: "Generic", amount: 10)
+                    expenses.items.append(expense)
+                }) {
+                   Image(systemName: "plus")
+                }
+            )
+            
+        }
+        
     }
+    
+    func deleteExpense(at index: IndexSet){
+        expenses.items.remove(atOffsets: index)
+    }
+    
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
